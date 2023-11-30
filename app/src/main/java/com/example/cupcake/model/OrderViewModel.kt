971a7324c2e018e77a232aef9145c8f2d8bb4f94
Priti Jain <pritik.jain@gmail.com>
2023-11-30 11:09:02 -0800

    Initial commit

---
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,94 @@
+package com.example.cupcake.model
+
+/*
+Order quantity (Integer)
+Cupcake flavor (String)
+Pickup date (String)
+Price (Double)
+ */
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
+class OrderViewModel:ViewModel() {
+
+    private val _quantity = MutableLiveData<Int>()
+    val quantity: LiveData<Int> = _quantity
+
+    private val _flavor = MutableLiveData<String>()
+    val flavor: LiveData<String> = _flavor
+
+    private val _date = MutableLiveData<String>()
+    val date : LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>()
+    val price : LiveData<String> = Transformations.map(_price){
+        NumberFormat.getCurrencyInstance().format(it)
+    }
+
+    val dateOptions = getPickupOptions()
+
+    init {
+        resetOrder()
+    }
+
+    private fun updatePrice() {
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        // if the user selected the first option (today) for pickup, add the surcharge
+        if(dateOptions[0] == _date.value){
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
+    }
+
+    fun setQuantity(numberCupcakes: Int) {
+        _quantity.value = numberCupcakes
+        updatePrice()
+    }
+
+    fun setFlavor(flavor: String) {
+        _flavor.value = flavor
+    }
+
+    fun setDate(date: String)
+    {
+        _date.value = date
+        updatePrice()
+    }
+
+    fun hasNoFlavorSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        //create a list of dates starting with the current date and the following 3 dates
+        repeat(4){
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+
+
+
+}
